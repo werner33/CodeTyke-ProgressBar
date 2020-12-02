24fb30453180d60b65157a2712c4469f9bd60fe8
werner33 <jordanmmanley@gmail.com>
2020-12-01 21:27:17 -0500

    initial commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Styles.scss';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__content">
+        <small className="footer__copyright">&copy; Codetyke 2020</small>
+        <small className="footer__reserved">All rights reserved</small>
+        <small className="footer__slogan">Get ready to start coding!</small>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Styles.scss';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar__content">
+        <img alt="logo" src="assets/logo.png" />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
